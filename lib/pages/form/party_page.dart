46863accf5442b83e7ac6aff602853482c9d63f0
kilianrdgs <kilian.rdgs@free.fr>
2023-10-18 21:23:28 +0200

    page partie 1

--- lib/pages/form/party_page.dart
+++ lib/pages/form/party_page.dart
@@ -13,9 +13,74 @@ class FormPartyPage extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 247, 184, 247),
       ),
       body: Container(
-        color: const Color.fromARGB(255, 197, 224, 255),
-        child: const Center(child: Text('Formulaire soirée')),
-      ),
+          color: const Color.fromARGB(255, 197, 224, 255),
+          child: Column(
+            children: [
+              const SizedBox(
+                  height: 200,
+                  child: Card(
+                    child: Expanded(
+                      child: Center(
+                          child: Text(
+                        'Caroussel',
+                      )),
+                    ),
+                  )),
+              Expanded(
+                child: ListView(
+                  children: [
+                    ListTile(
+                      title: Card(
+                        color: Colors.white,
+                        child: Container(
+                            height: 60,
+                            margin: EdgeInsets.all(0),
+                            child: const Text(
+                                '(PDP) (prénom nom de linvité), au clic, invité sera ajt')),
+                      ),
+                    ),
+                    ListTile(
+                      title: Card(
+                        color: Colors.white,
+                        child: Container(
+                            height: 60,
+                            margin: EdgeInsets.all(0),
+                            child: const Text(
+                                '(PDP) (prénom nom de linvité), au clic, invité sera ajt')),
+                      ),
+                    ),
+                    ListTile(
+                      title: Card(
+                        color: Colors.white,
+                        child: Container(
+                            height: 60,
+                            margin: EdgeInsets.all(0),
+                            child: const Text(
+                                '(PDP) (prénom nom de linvité), au clic, invité sera ajt')),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              FractionallySizedBox(
+                child: SizedBox(
+                  height: 100,
+                  child: Center(
+                    child: ElevatedButton(
+                        onPressed: () {
+                          //action du formulaire ici
+                        },
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 129, 68, 187)),
+                        child: const Text("Créer la soirée 🎉",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20))),
+                  ),
+                ),
+              )
+            ],
+          )),
       bottomNavigationBar: const CustomBottomNavigationBar(),
     );
   }
