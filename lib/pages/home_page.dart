a51c8ed997e16f89112008c5025253b8ad716eb9
Isline666 <nils.dargent@gmail.com>
2023-10-19 14:54:12 +0200

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mlp/class/event.dart';
+import 'package:flutter_mlp/widgets/events_list.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  late List<Event>? _events;
+
+  bool _eventsDataLoaded = false;
+
+  Future<void> _getEvents() async {
+    List<Event>? eventsList = await Events.getEvents();
+    setState(() {
+      _events = eventsList;
+      _eventsDataLoaded = true;
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 10), () async{
+      await _getEvents();
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: const Text('Home'),),
+      body: _eventsDataLoaded
+          ? EventListWidget(eventList: _events)
+          :const Center(),
     );
   }
 }
