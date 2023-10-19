79faccbc847a2166d5a24f60488c1886851ba756
YimoSsDev <116626946+YimoSsDev@users.noreply.github.com>
2023-10-19 20:57:42 +0200

    Update 1.9.2

--- lib/pages/riders_page.dart
+++ lib/pages/riders_page.dart
@@ -1,11 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mlp/database/db_class.dart';
+import 'package:flutter_mlp/class/rider.dart';
 
-class RidersPage extends StatelessWidget {
-  const RidersPage({super.key});
+class RidersPage extends StatefulWidget {
+  @override
+  _RidersPageState createState() => _RidersPageState();
+}
+
+class _RidersPageState extends State<RidersPage> {
+  List<Rider> riders = [];
+  DbConnect myDb = DbConnect();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchRidersFromMongoDB();
+  }
+
+  void fetchRidersFromMongoDB() async {
+    try {
+      // Assurez-vous d'utiliser la méthode correcte pour récupérer les cavaliers depuis MongoDB
+      List<Rider> fetchedRiders = (await myDb.getRiders()).cast<Rider>();
+
+      setState(() {
+        riders = fetchedRiders;
+      });
+    } catch (e) {
+      print("Erreur lors de la récupération des cavaliers : $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: const Text('List'),),
+      appBar: AppBar(
+        title: Text('Liste des Cavaliers'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add), // Bouton pour accéder à la création de cavaliers
+            onPressed: () {
+              Navigator.pushNamed(context, '/create_rider'); // Redirigez vers la page de création de cavaliers
+            },
+          ),
+        ],
+      ),
+      body: riders.isNotEmpty
+          ? ListView.builder(
+        itemCount: riders.length,
+        itemBuilder: (context, index) {
+          Rider rider = riders[index];
+          return ListTile(
+            title: Text(rider.name),
+            subtitle: Text('Age: ${rider.age.toString()}'),
+          );
+        },
+      )
+          : Center(
+        child: Text('Aucun cavalier trouvé'), // Message si la liste est vide
+      ),
     );
-}}
+  }
+}
