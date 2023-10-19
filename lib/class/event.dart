20aa71fedef3415adc9c8402b4ee44be9a85ba7b
E.Delalande <116553314+rat-balls@users.noreply.github.com>
2023-10-20 00:20:55 +0200

    Merged all events.dart

--- lib/class/event.dart
+++ lib/class/event.dart
@@ -16,47 +16,42 @@ class Event {
   List<dynamic>? horses;
   String? discipline;
   int? duree;
-  bool etat;
+  bool etat = false;
 
-  Event(
-      {this.id,
-      required this.type,
-      this.users,
-      required this.date,
-      required this.creator,
-      required this.place,
-      this.horses,
-      required this.discipline,
-      this.duree,
-      required this.etat});
+  Event({this.id,
+    required this.type,
+    this.users,
+    required this.date,
+    this.time,
+    required this.creator,
+    required this.place,
+    this.horses,
+    required this.discipline,
+    this.duree,
+    this.etat = false
+  });
 
-    static Future<List<Event>?> getAllEvents() async {
-      var db = DbConnect().dbref;
-      var eventCollection = db.collection('Events');
-      List<Event> eventList = [];
-      Event eventInfo;
-      try {
-        var eventsData = await eventCollection.find().toList();
-        for (var event in eventsData) {
-            eventInfo = Event(
-                id: event['_id'],
-                type: event['type'],
-                users: List<ObjectId>.from(event['users']),
-                date: event['date'],
-                creator: event['creator'],
-                place: event?['place'],
-                horses: List<ObjectId>.from(event['horses']),
-                discipline: event['discipline'],
-                duree: event['duree'],
-                etat: event['etat']
-            );
-            eventList.add(eventInfo);
-        }
-        print(eventList);
-        return eventList;
-      } catch (e) {
-        print("Erreur lors de la récupération des données : $e");
-        return eventList;
+  static Future<List<Event>?> getAllEvents() async {
+    var db = DbConnect().dbref;
+    var eventCollection = db.collection('Events');
+    List<Event> eventList = [];
+    Event eventInfo;
+    try {
+      var eventsData = await eventCollection.find().toList();
+      for (var event in eventsData) {
+        eventInfo = Event(
+            id: event['_id'],
+            type: event['type'],
+            users: List<ObjectId>.from(event['users']),
+            date: event['date'],
+            creator: event['creator'],
+            place: event?['place'],
+            horses: List<ObjectId>.from(event['horses']),
+            discipline: event['discipline'],
+            duree: event['duree'],
+            etat: event['etat']
+        );
+        eventList.add(eventInfo);
       }
       print(eventList);
       return eventList;
@@ -65,7 +60,6 @@ class Event {
       return eventList;
     }
   }
-
 //-----------------------------------------------------------------------
 //PROGRAMMER COMPETITION
 
@@ -136,6 +130,8 @@ class Event {
   }
 //------------------------------------------------------------------------
 
+
+
   static Future<Map<DateTime, List<Event>>?> getAllEventsTimed() async {
     var db = DbConnect().dbref;
     var eventCollection = db.collection('Events');
@@ -172,4 +168,65 @@ class Event {
       return eventMap;
     }
   }
+}
+
+static Future<List<Event>?> getValideEvents() async {
+var db = DbConnect().dbref;
+var eventCollection = db.collection('Events');
+List<Event> eventList = [];
+Event eventInfo;
+try {
+var eventsData = await eventCollection.find().toList();
+
+for (var event in eventsData) {
+if (event['etat'] == true){
+eventInfo = Event(
+id: event['_id'],
+type: event['type'],
+users: List<ObjectId>.from(event['users']),
+date: event['date'],
+creator: event['creator'],
+place: event['place'],
+horses: List<ObjectId>.from(event['horses']),
+discipline: event['discipline'],
+duree: event['duree'],
+etat: event['etat']
+);
+eventList.add(eventInfo);
+};
+
+}
+print(eventList);
+return eventList;
+} catch (e) {
+print("Erreur lors de la récupération des données : $e");
+return eventList;
+}
+}
+
+static Future<void> acceptEvent(ObjectId? eventId) async{
+var db = DbConnect().dbref;
+var eventCollection = db.collection('Events');
+try {
+await eventCollection.update(
+where.eq('_id', eventId),
+modify
+    .set('etat', true));
+print('Evenement accepter');
+} catch (e){
+print('Erreur lors de la mise à jour : $e');
+}
+}
+
+static Future<void> deleteEvent(ObjectId? eventId) async{
+var db = DbConnect().dbref;
+var eventCollection = db.collection('Events');
+try {
+await eventCollection.deleteOne(
+where.eq('_id', eventId));
+print('Evenement supprimer');
+} catch (e){
+print('Erreur lors de la mise à jour : $e');
+}
+}
 }
