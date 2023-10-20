e67cd035ccef8d8c72cc2fd210540e848176f958
E.Delalande <116553314+rat-balls@users.noreply.github.com>
2023-10-20 12:45:48 +0200

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -32,12 +32,13 @@ class _HomePageState extends State<HomePage> {
 
   Future<void> _getEvents() async {
     List<Event>? valideEventsList = await Event.getValideEvents();
-
-    /*List<Event>? eventsList = await Event.getAllEvents();
-    await Event.acceptEvent(eventsList?[6].id);
-    await Event.acceptEvent(eventsList?[7].id);
-    await Event.deleteEvent(eventsList?[7].id);*/
-    //Test pour accepter et supprimer les events
+    valideEventsList?.forEach((event) {
+      if(event.date.isBefore(DateTime.now())) {
+        Event.deleteEvent(event.id);
+      }
+    });
+    valideEventsList = await Event.getValideEvents();
+    valideEventsList?.sort((e1, e2) => e1.date.compareTo(e2.date));
     setState(() {
       _valideEvents = valideEventsList;
       _eventsDataLoaded = true;
@@ -48,7 +49,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
 
-    Future.delayed(const Duration(seconds: 3), () async {
+    Future.delayed(const Duration(seconds: 5), () async {
       await _getEvents();
       await _getUserInfo();
       print(checkAdmin);
